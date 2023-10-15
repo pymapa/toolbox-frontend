bce851fa82d8f8907051616618a52d04a4220016
Pyry Pajunpää <pyry.pajunpaa@op.fi>
2023-10-15 23:15:22 +0300

    Add tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<div class="flex flex-wrap h-screen bg-slate-900">
+
+	<div class="h-12 w-full bg-white">
+		<h2>nav</h2>
+	</div>
+	<slot />
+</div>
